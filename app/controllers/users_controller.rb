d9eb6ef9e25fd9000fe041a79d04deaf0e8fa20e
Jeremy Smith <jeremy@323media.com>
2010-10-13 00:03:34 -0400

    limit lols returned on user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,10 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find_by_screen_name(params[:screen_name])
+    @received_lols_count = @user.received_lols.count
+    @received_lols = @user.received_lols.find(:all, :limit => 50, :order => 'created_at desc')
+    @given_lols_count = @user.given_lols.count
+    @given_lols = @user.given_lols.find(:all, :limit => 50, :order => 'created_at desc')
   end
   
 end
