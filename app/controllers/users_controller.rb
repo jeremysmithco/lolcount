59a9efb9df160eafa6a150479c35f931059eac69
Jeremy Smith <jeremy@323media.com>
2011-02-22 00:41:09 -0500

    changed index to use counter cache for sort

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   
   def index
-    @users = User.find(:all, :joins => :received_lols, :select => 'users.*, count(lols.lolee_id) as lol_count', :group => 'users.id', :limit => 100, :order => 'lol_count desc')
+    @users = User.find(:all, :limit => 100, :order => 'received_lol_count desc')
   end
   
   def show
